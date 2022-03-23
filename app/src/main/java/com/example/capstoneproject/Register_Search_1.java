34bfe909c2b32b1a27069011d837a7c6013fe12a
xancxoxin <judy010321@naver.com>
2022-03-23 14:51:31 +0900

    Initial commit

---
+++ app/src/main/java/com/example/capstoneproject/Register_Search_1.java
@@ -0,0 +1,14 @@
+package com.example.capstoneproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Register_Search_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+    }
+}
