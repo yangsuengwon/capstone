cde3562b0a8c76c1fbc931157b8bb9ace8c20147
peachpuding <psjwww0411@naver.com>
2022-03-23 21:54:43 +0900

    메인과 등록 페이지 연결등
    Glide 디펜던시 삽입(URL 그림삽입목적)
    에러 일부 수정

--- app/src/main/java/com/example/capstoneproject/Home.java
+++ app/src/main/java/com/example/capstoneproject/Home.java
@@ -2,13 +2,27 @@ package com.example.capstoneproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Home extends AppCompatActivity {
-
+    Button registration;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_1);
+
+        registration = findViewById(R.id.button3);
+
+        registration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Home.this,MainActivity.class);
+
+                startActivity(intent);
+            }
+        });
     }
 }
