912a2f0dacf5aee7b73edeb97845072eb373d75e
peachpuding <psjwww0411@naver.com>
2022-03-30 16:16:18 +0900

    종료 버튼 XML 에러수정

--- app/src/main/java/com/example/capstoneproject/Home.java
+++ app/src/main/java/com/example/capstoneproject/Home.java
@@ -1,14 +1,19 @@
 package com.example.capstoneproject;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class Home extends AppCompatActivity {
     Button registration;
+
+    ImageButton shutdown;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,12 +21,39 @@ public class Home extends AppCompatActivity {
 
         registration = findViewById(R.id.button3);
 
+        shutdown = findViewById(R.id.imageButton3);
+
         registration.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(Home.this,MainActivity.class);
+                Intent intent = new Intent(Home.this,Register_Search_1.class);
 
                 startActivity(intent);
+            }
+        });
+
+        shutdown.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                AlertDialog.Builder builder = new AlertDialog.Builder(Home.this);
+                builder.setMessage("정말로 종료하시겠습니까?");
+                builder.setTitle("종료 알림창")
+                        .setCancelable(false)
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int i) {
+                                finish();
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int i) {
+                                dialog.cancel();
+                            }
+                        });
+                AlertDialog alert = builder.create();
+                alert.setTitle("종료 알림창");
+                alert.show();
             }
         });
     }
