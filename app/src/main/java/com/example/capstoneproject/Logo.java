d7a4ee3ee73fee390b0a4ba5b03f1dab338b8c17
박성진 <psjwww0411@naver.com>
2022-04-13 16:32:12 +0900

    xml 일부 수정 오류 수정 켈린더 팝업
    파일 IO 개발중

--- app/src/main/java/com/example/capstoneproject/Logo.java
+++ app/src/main/java/com/example/capstoneproject/Logo.java
@@ -3,19 +3,14 @@ package com.example.capstoneproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-<<<<<<< HEAD:app/src/main/java/com/example/capstoneproject/MainActivity.java
-=======
 import android.widget.Button;
->>>>>>> master:app/src/main/java/com/example/capstoneproject/Logo.java
+
 
 public class Logo extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-<<<<<<< HEAD:app/src/main/java/com/example/capstoneproject/MainActivity.java
-        setContentView(R.layout.activity_main);
-=======
         setContentView(R.layout.activity_logo);
 
         /*button = findViewById(R.id.button);
@@ -28,6 +23,5 @@ public class Logo extends AppCompatActivity {
                 startActivity(intent);
             }
         });*/
->>>>>>> master:app/src/main/java/com/example/capstoneproject/Logo.java
     }
 }
