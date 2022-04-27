0bc4abdd090579b895b28e8618689ea39a66a3e9
xancxoxin <judy010321@naver.com>
2022-04-27 15:20:03 +0900

    2022_04_22

---
+++ app/src/main/java/com/example/capstoneproject/Product_Register_Popup.java
@@ -0,0 +1,25 @@
+package com.example.capstoneproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.widget.DatePicker;
+import android.widget.Toast;
+
+import java.util.Calendar;
+
+public class Product_Register_Popup extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_popup);
+
+        //image button 클릭시 popup으로 달력 뜨게 해주세요.
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/capstoneproject/Repair_Register.java
@@ -0,0 +1,14 @@
+package com.example.capstoneproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Repair_Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_repair_register);
+    }
+}
