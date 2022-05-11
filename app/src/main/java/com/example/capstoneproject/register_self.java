3517b78e184f81b246b44a5805f843dfeeee5936
yang <yang2381@naver.com>
2022-05-11 14:38:05 +0900

    calender_popup

--- app/src/main/java/com/example/capstoneproject/register_self.java
+++ app/src/main/java/com/example/capstoneproject/register_self.java
@@ -2,14 +2,25 @@ package com.example.capstoneproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class register_self extends AppCompatActivity {
 
     Button bt_registration;
+    ImageView imgbt_calender;
+    DatePickerDialog datePickerDialog;
+    String date;
+    TextView tv_selfdate;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +28,8 @@ public class register_self extends AppCompatActivity {
         setContentView(R.layout.activity_register_self);
 
         bt_registration = findViewById(R.id.button6);
+        imgbt_calender=findViewById(R.id.self_calender);
+        tv_selfdate=findViewById(R.id.tv_selfdate);
 
         bt_registration.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,6 +37,27 @@ public class register_self extends AppCompatActivity {
                 Intent intent = new Intent(register_self.this,Home.class);
 
                 startActivity(intent);
+            }
+        });
+        imgbt_calender.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar calendar = Calendar.getInstance();
+                int nYear = calendar.get(Calendar.YEAR);
+                int nMonth = calendar.get(Calendar.MONTH);
+                int nDay = calendar.get(Calendar.DAY_OF_MONTH);
+
+                datePickerDialog = new DatePickerDialog(register_self.this,
+                        new DatePickerDialog.OnDateSetListener(){
+                            @Override
+                            public void onDateSet(DatePicker datePicker, int year, int month, int day){
+                                month+=1;
+                                date=year+"/"+month+"/"+day;
+                                tv_selfdate.setText(date);
+                            }
+
+                        },nYear,nMonth,nDay);
+                datePickerDialog.show();
             }
         });
     }
