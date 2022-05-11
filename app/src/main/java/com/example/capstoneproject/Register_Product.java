3053d2a8112d22f33c0a7de8ec8e1456bfec47bc
yang <yang2381@naver.com>
2022-05-11 14:14:17 +0900

    calender_popup

--- app/src/main/java/com/example/capstoneproject/Register_Product.java
+++ app/src/main/java/com/example/capstoneproject/Register_Product.java
@@ -10,56 +10,52 @@ import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.ImageButton;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.Calendar;
 
 public class Register_Product extends AppCompatActivity {
 
     Button bt_submit;
-    TextView tv_date;
     ImageButton imgbt_calender;
+    TextView tv_date;
+    DatePickerDialog datePickerDialog;
+    String date;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_product);
 
-        imgbt_calender = findViewById(R.id.bt_calender);
-
+        tv_date=findViewById(R.id.tv_date);
+        imgbt_calender=findViewById(R.id.bt_calender);
         bt_submit = findViewById(R.id.button);
-
-        int year1 = 0,month1 = 0,day1 = 0;
-
-        imgbt_calender.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(Register_Product.this,Product_Register_Popup.class);
-                startActivity(intent);
-            }
-        });
-
-        DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                tv_date.setText(dayOfMonth+"/" + (month+1) + "/" + year);
-            }
-        }
-        , year1, month1, day1);
-        imgbt_calender.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                datePickerDialog.show();
-            }
-        });
 
 
         bt_submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-
-
                 Intent intent = new Intent(Register_Product.this,Home.class);
+            }
+        });
+        imgbt_calender.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar calendar = Calendar.getInstance();
+                int nYear = calendar.get(Calendar.YEAR);
+                int nMonth = calendar.get(Calendar.MONTH);
+                int nDay = calendar.get(Calendar.DAY_OF_MONTH);
+
+                datePickerDialog = new DatePickerDialog(Register_Product.this,
+                        new DatePickerDialog.OnDateSetListener(){
+                            @Override
+                            public void onDateSet(DatePicker datePicker,int year,int month,int day){
+                                month+=1;
+                                date=year+"/"+month+"/"+day;
+                                tv_date.setText(date);
+                            }
+
+                        },nYear,nMonth,nDay);
+                datePickerDialog.show();
             }
         });
     }
