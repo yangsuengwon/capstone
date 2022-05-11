f9e07c18c06d09f0ec9b10e3de79df0e2485ddd2
yang <yang2381@naver.com>
2022-05-11 15:40:36 +0900

    calender_popup

--- app/src/main/java/com/example/capstoneproject/register_self.java
+++ app/src/main/java/com/example/capstoneproject/register_self.java
@@ -19,6 +19,7 @@ public class register_self extends AppCompatActivity {
     ImageView imgbt_calender;
     DatePickerDialog datePickerDialog;
     String sDate;
+    int n;
     TextView tv_selfdate;
 
 
